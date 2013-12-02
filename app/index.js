fd9dd11a1621da948323094cb18c9160a3c3b6ce
TJ <tj@server1.bisonpc.net>
2013-12-02 07:16:34 +0400

    First Commit

---
+++ app/index.js
@@ -0,0 +1,126 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var spawn = require('child_process').spawn;
+var yeoman = require('yeoman-generator');
+
+
+var AppGenerator = module.exports = function Appgenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  // setup the test-framework property, Gruntfile template will need this
+  this.testFramework = options['test-framework'] || 'mocha';
+
+  // for hooks to resolve on mocha by default
+  if (!options['test-framework']) {
+    options['test-framework'] = 'mocha';
+  }
+
+  // resolved to mocha by default (could be switched to jasmine for instance)
+  this.hookFor('test-framework', { as: 'app' });
+
+  this.on('end', function () {
+    this.installDependencies({
+      skipInstall: options['skip-install'],
+      skipMessage: options['skip-install-message']
+    });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(AppGenerator, yeoman.generators.Base);
+
+AppGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // welcome message
+  if (!this.options['skip-welcome-message']) {
+    console.log(this.yeoman);
+    console.log('Out of the box I include HTML5 Boilerplate and jQuery.');
+  }
+
+  var prompts = [{
+    type: 'checkbox',
+    name: 'features',
+    message: 'What more would you like?',
+    choices: [{
+      name: 'Modernizr',
+      value: 'includeModernizr',
+      checked: true
+    }]
+  },{
+    type: 'input',
+    name: 'host_name',
+    message: 'What address would you like to bind to (defaults to "localhost", choose 0.0.0.0 to bind to every IP on the default interface)?',
+    default: 'localhost'
+  }];
+
+  this.prompt(prompts, function (answers) {
+    var features = answers.features;
+
+    function hasFeature(feat) { return features.indexOf(feat) !== -1; }
+
+    // manually deal with the response, get back and store the results.
+    // we change a bit this way of doing to automatically do this in the self.prompt() method.
+    this.includeModernizr = hasFeature('includeModernizr');
+    this.host = answers.host_name;
+    cb();
+  }.bind(this));
+};
+
+AppGenerator.prototype.gruntfile = function gruntfile() {
+  this.template('Gruntfile.js');
+};
+
+AppGenerator.prototype.packageJSON = function packageJSON() {
+  this.template('_package.json', 'package.json');
+};
+
+AppGenerator.prototype.git = function git() {
+  this.copy('gitignore', '.gitignore');
+  this.copy('gitattributes', '.gitattributes');
+};
+
+AppGenerator.prototype.bower = function bower() {
+  this.copy('bowerrc', '.bowerrc');
+  this.copy('_bower.json', 'bower.json');
+};
+
+AppGenerator.prototype.jshint = function jshint() {
+  this.copy('jshintrc', '.jshintrc');
+};
+
+AppGenerator.prototype.editorConfig = function editorConfig() {
+  this.copy('editorconfig', '.editorconfig');
+};
+
+AppGenerator.prototype.h5bp = function h5bp() {
+  this.copy('favicon.ico', 'app/favicon.ico');
+  this.copy('404.html', 'app/404.html');
+  this.copy('robots.txt', 'app/robots.txt');
+  this.copy('htaccess', 'app/.htaccess');
+};
+
+AppGenerator.prototype.mainStylesheet = function mainStylesheet() {
+    this.copy('main.css', 'app/styles/main.css');
+};
+
+AppGenerator.prototype.writeIndex = function writeIndex() {
+
+  this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
+  this.indexFile = this.engine(this.indexFile, this);
+  this.indexFile = this.appendScripts(this.indexFile, 'scripts/main.js', [
+    'scripts/main.js'
+  ]);
+};
+
+AppGenerator.prototype.app = function app() {
+  this.mkdir('app');
+  this.mkdir('app/scripts');
+  this.mkdir('app/styles');
+  this.mkdir('app/images');
+  this.write('app/index.html', this.indexFile);
+
+  this.write('app/scripts/main.js', 'console.log(\'\\\'Allo \\\'Allo!\');');
+};
